12dc80604f2e7d49c058d1347b5b53d118e3a955
andrewdef1 <andrewbelanda@gmail.com>
2022-07-07 20:01:19 +0900

    yees

--- application/views/inventaris/masuk.php
+++ application/views/inventaris/masuk.php
@@ -68,7 +68,7 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <input type="hidden" name="kode_barang" id="kode_barang" value="KB">
-								<input type="text" name="id_barang" id="id_barang" value="<?=  $kdBrg; ?>">
+								<input type="hidden" name="id_barang" id="id_barang" value="<?=  $kdBrg; ?>">
                                 <input type="hidden" name="hash" id="hash" value="">
                                 <div class="form-group">
                                     <label for="date">Tanggal *</label>
