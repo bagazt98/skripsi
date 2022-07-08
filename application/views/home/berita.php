8b2bf451bed71e7d5787d6843b58118d8eafc86d
bagazt98 <97887652+bagazt98@users.noreply.github.com>
2022-07-08 09:17:31 +0700

    update fix img post

--- application/views/home/berita.php
+++ application/views/home/berita.php
@@ -1,20 +1,20 @@
 <div class="card mb-3 style=" width: 18rem;>
-    <div class="row mt-4">
-        <?php $i = 1; ?>
-        <?php foreach ($brt as $s) : ?>
-            <div class="col-md-4 mt-5" data-wow-delay="0.1s">
-                <div class="card shadow-sm">
-                    <a href="<?= base_url('home/post/') . $s['id']; ?>"><img src="<?= base_url('assets/img/berita/') . $s['gambar']; ?>" class="card-img-top" alt=""></a>
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $s['judul_berita']; ?></h5>
-                        <!-- <p class="card-text"><small class="text-muted"><?= $s['name']; ?></small></p> -->
-                    </div>
-                </div>
-            </div>
-            <?php $i++; ?>
+	<div class="row mt-4">
+		<?php $i = 1; ?>
+		<?php foreach ($brt as $s) : ?>
+			<div class="col-md-4 mt-5" data-wow-delay="0.1s">
+				<div class="card shadow-sm">
+					<a href="<?= base_url('home/post/') . $s['id']; ?>"><img max-width="80%" height="350px" width="250px" src=" <?= base_url('assets/img/berita/') . $s['gambar']; ?>" class="card-img-top" alt=""></a>
+					<div class="card-body">
+						<h5 class="card-title"><?= $s['judul_berita']; ?></h5>
 
-        <?php endforeach; ?>
+					</div>
+				</div>
+			</div>
+			<?php $i++; ?>
 
-    </div>
+		<?php endforeach; ?>
+
+	</div>
 </div>
 <!-- Postingan End -->
--- application/views/home/home.php
+++ application/views/home/home.php
@@ -91,7 +91,7 @@
 			<?php foreach ($brt as $s) : ?>
 				<div class="col-md-4 mt-5" data-wow-delay="0.1s">
 					<div class="card shadow-sm">
-						<a href="<?= base_url('home/post/') . $s['id']; ?>"><img src="<?= base_url('assets/img/berita/') . $s['gambar']; ?>" class="card-img-top" alt=""></a>
+						<a href="<?= base_url('home/post/') . $s['id']; ?>"><img max-width="80%" height="350px" width="250px" src="<?= base_url('assets/img/berita/') . $s['gambar']; ?>" class="card-img-top" alt=""></a>
 						<div class="card-body">
 							<h5 class="card-title"><?= $s['judul_berita']; ?></h5>
 							<p class="card-text"><small class="text-muted"><?= $s['name']; ?></small></p>
